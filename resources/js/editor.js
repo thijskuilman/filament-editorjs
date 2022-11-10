31a34e39a41bca821b9a6f3382622838675104bc
Thijs Kuilman <thijs.kuilman@gmail.com>
2022-11-10 11:17:14 +0100

    Update editor.js

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -14,125 +14,125 @@ import DragDrop from "editorjs-drag-drop";
 import Embed from '@editorjs/embed';
 
 export default class CustomEditorJSEmbed extends Embed {
-    render() {
-        this.element = Embed.prototype.render.call(this);
+  render() {
+    this.element = Embed.prototype.render.call(this);
 
-        if (this.element.querySelector('.embed-tool__caption')) {
-            this.element.querySelector('.embed-tool__caption').remove();
-        }
-        return this.element;
+    if (this.element.querySelector('.embed-tool__caption')) {
+      this.element.querySelector('.embed-tool__caption').remove();
     }
+    return this.element;
+  }
 }
 
 document.addEventListener("alpine:init", () => {
-    Alpine.data(
-        "editorjs",
-        ({state, statePath, placeholder, readOnly, tools, minHeight}) => ({
-            instance: null,
-            state: state,
-            tools: tools,
-            init() {
-                let enabledTools = {};
+  Alpine.data(
+      "editorjs",
+      ({state, statePath, placeholder, readOnly, tools, minHeight}) => ({
+        instance: null,
+        state: state,
+        tools: tools,
+        init() {
+          let enabledTools = {};
 
-                if (this.tools.includes("header")) enabledTools.header = Header;
+          if (this.tools.includes("header")) enabledTools.header = Header;
 
-                if (this.tools.includes("image")) {
-                    enabledTools.image = {
-                        class: ImageTool,
-                        config: {
-                            uploader: {
-                                uploadByFile: (file) => {
-                                    return new Promise((resolve) => {
-                                        this.$wire.upload(
-                                            `componentFileAttachments.${statePath}`,
-                                            file,
-                                            (uploadedFilename) => {
-                                                this.$wire
-                                                    .getComponentFileAttachmentUrl(statePath)
-                                                    .then((url) => {
-                                                        if (!url) {
-                                                            return resolve({
-                                                                success: 0,
-                                                            });
-                                                        }
-                                                        return resolve({
-                                                            success: 1,
-                                                            file: {
-                                                                url: url,
-                                                            },
-                                                        });
-                                                    });
-                                            }
-                                        );
+          if (this.tools.includes("image")) {
+            enabledTools.image = {
+              class: ImageTool,
+              config: {
+                uploader: {
+                  uploadByFile: (file) => {
+                    return new Promise((resolve) => {
+                      this.$wire.upload(
+                          `componentFileAttachments.${statePath}`,
+                          file,
+                          (uploadedFilename) => {
+                            this.$wire
+                                .getComponentFileAttachmentUrl(statePath)
+                                .then((url) => {
+                                  if (!url) {
+                                    return resolve({
+                                      success: 0,
                                     });
-                                },
+                                  }
+                                  return resolve({
+                                    success: 1,
+                                    file: {
+                                      url: url,
+                                    },
+                                  });
+                                });
+                          }
+                      );
+                    });
+                  },
 
-                                uploadByUrl: (url) => {
-                                    return this.$wire.loadImageFromUrl(url).then((result) => {
-                                        return {
-                                            success: 1,
-                                            file: {
-                                                url: result,
-                                            },
-                                        };
-                                    });
-                                },
-                            },
+                  uploadByUrl: (url) => {
+                    return this.$wire.loadImageFromUrl(url).then((result) => {
+                      return {
+                        success: 1,
+                        file: {
+                          url: result,
                         },
-                    };
+                      };
+                    });
+                  },
+                },
+              },
+            };
+          }
+          if (this.tools.includes("delimiter")) enabledTools.delimiter = Delimiter;
+          if (this.tools.includes("list")) enabledTools.list = List;
+          if (this.tools.includes("underline")) enabledTools.underline = Underline;
+          if (this.tools.includes("quote")) enabledTools.quote = Quote;
+          if (this.tools.includes("table")) enabledTools.table = Table;
+          if (this.tools.includes("raw")) enabledTools.raw = RawTool;
+          if (this.tools.includes("code")) enabledTools.code = Code;
+          if (this.tools.includes("inline-code")) enabledTools.inlineCode = InlineCode;
+          if (this.tools.includes("style")) enabledTools.style = StyleInlineTool;
+          if (this.tools.includes("style")) enabledTools.style = StyleInlineTool;
+
+          if (this.tools.includes("embed")) {
+
+            enabledTools.embed = {
+              class: CustomEditorJSEmbed,
+              config: {
+                services: {
+                  youtube: true,
+                  coub: true,
+                  collectible: {
+                    regex: new RegExp('http[s]?:\/\/' + window.location.host + '\/.*\/collectibles\/([^\/\?\&]+)?.*'),
+                    embedUrl: window.location.origin + '/en/collectibles/<%= remote_id %>/embed',
+                    html: "<iframe height='280px' scrolling='no' frameborder='no' allowtransparency='true' style='width: 100%;'></iframe>",
+                    height: 300,
+                    width: 600,
+                    caption: false,
+                    id: (groups) => groups.join('/embed/')
+                  }
                 }
-                if (this.tools.includes("delimiter")) enabledTools.delimiter = Delimiter;
-                if (this.tools.includes("list")) enabledTools.list = List;
-                if (this.tools.includes("underline")) enabledTools.underline = Underline;
-                if (this.tools.includes("quote")) enabledTools.quote = Quote;
-                if (this.tools.includes("table")) enabledTools.table = Table;
-                if (this.tools.includes("raw")) enabledTools.raw = RawTool;
-                if (this.tools.includes("code")) enabledTools.code = Code;
-                if (this.tools.includes("inline-code")) enabledTools.inlineCode = InlineCode;
-                if (this.tools.includes("style")) enabledTools.style = StyleInlineTool;
-                if (this.tools.includes("style")) enabledTools.style = StyleInlineTool;
+              }
+            }
+          }
+          ;
 
-                if (this.tools.includes("embed")) {
+          this.instance = new EditorJS({
+            holder: this.$el,
+            minHeight: minHeight,
+            data: this.state,
+            placeholder: placeholder,
+            readOnly: readOnly,
+            tools: enabledTools,
 
-                    enabledTools.embed = {
-                        class: CustomEditorJSEmbed,
-                        config: {
-                            services: {
-                                youtube: true,
-                                coub: true,
-                                collectible: {
-                                    regex: new RegExp('http[s]?:\/\/' + window.location.host + '\/.*\/collectibles\/([^\/\?\&]+)?.*'),
-                                    embedUrl: window.location.origin + '/en/collectibles/<%= remote_id %>/embed',
-                                    html: "<iframe height='280px' scrolling='no' frameborder='no' allowtransparency='true' style='width: 100%;'></iframe>",
-                                    height: 300,
-                                    width: 600,
-                                    caption: false,
-                                    id: (groups) => groups.join('/embed/')
-                                }
-                            }
-                        }
-                    }
-                }
-                ;
-
-                this.instance = new EditorJS({
-                    holder: this.$el,
-                    minHeight: minHeight,
-                    data: this.state,
-                    placeholder: placeholder,
-                    readOnly: readOnly,
-                    tools: enabledTools,
-
-                    onChange: () => {
-                        this.instance.save().then((outputData) => {
-                            this.state = outputData;
-                        });
-                    },
-                    onReady: () => {
-                        new DragDrop(this.instance);
-                    },
-                });
+            onChange: () => {
+              this.instance.save().then((outputData) => {
+                this.state = outputData;
+              });
             },
-        })
-    );
+            onReady: () => {
+              new DragDrop(this.instance);
+            },
+          });
+        },
+      })
+  );
 });
